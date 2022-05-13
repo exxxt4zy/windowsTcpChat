c91ec4ef2be844c5333f681668cd29621a7fd4bd
exxxt4zy <78819104+exxxt4zy@users.noreply.github.com>
2022-05-13 21:22:23 +0500

    Add files via upload

---
+++ ChatClient.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+    class ChatClient
+    {
+        static string userName;
+        private const string host = "127.0.0.1";
+        private const int port = 8888;
+        static TcpClient client;
+        static NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+            ConsoleT("Локальный чат");
+            Console.Write("Введите имя: ");
+            userName = Console.ReadLine();
+            ConsoleT("Локальный чат: " + userName);
+            
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                // запуск нового потока для получения каких-то данных
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+                Console.WriteLine("Добро пожаловать, {0}", userName);
+                SendMessage();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+        static void ConsoleT(string title)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Title = title;
+        }
+        static void SendMessage()
+        {
+            Console.Write("Введите сообщение: ");
+            string message = Console.ReadLine();
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            message = null; 
+        }
+        static void ReceiveMessage()
+        {
+            while(true)
+            {
+                try
+                {
+                    byte[] data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    while(true)
+                    {
+                        string message = builder.ToString();
+                        Console.WriteLine(message);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение преравно!");
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();
+            if (client != null)
+                client.Close();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ ChatServer.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static ServerObject server;
+        static Thread listenThread;
+        static void Main(string[] args)
+        {
+            ConsoleT("Server Chat v1.0");
+            try
+            {
+                server = new ServerObject();
+                listenThread = new Thread(new ThreadStart(server.Listen));
+                listenThread.Start(); //старт потока
+            }
+            catch (Exception ex)
+            {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void ConsoleT(string title)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Title = title;
+        }
+    }
+    
+    public class ClientObject
+    {
+        protected internal string Id { get; private set; }
+        protected internal NetworkStream Stream { get; set; }
+        string userName;
+        TcpClient client;
+        ServerObject server;
+        public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+        {
+            Id = Guid.NewGuid().ToString();
+            client = tcpClient;
+            server = serverObject;
+            serverObject.AddConnection(this);
+        }
+        public void Process()
+        {
+            Stream = client.GetStream();
+            // получаем имя пользователя
+            string message = GetMessage();
+            userName = message;
+
+            message = userName + " вошёл в чат";
+            server.BroadcastMessage(message, this.Id);
+            Console.Beep();
+            Console.WriteLine(message);
+            while (true)
+            {
+                try
+                {
+                    message = GetMessage();
+                    message = string.Format("{0}: {1}", userName, message);
+                    Console.WriteLine(message);
+                    server.BroadcastMessage(message, this.Id);
+                }
+                catch
+                {
+                    message = String.Format("{0}: покинул чат", userName);
+                    Console.WriteLine(message);
+                    Console.Beep();
+                    server.BroadcastMessage(message, this.Id);
+                    break;
+                }
+                finally
+                {
+                    server.RemoveConnection(this.Id);
+                    Close();
+                }
+            }
+        }
+        private string GetMessage()
+        {
+            byte[] data = new byte[64]; // буфер для получаемых данных
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+            do
+            {
+                bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (Stream.DataAvailable);
+
+            return builder.ToString();
+        }
+        protected internal void Close()
+        {
+            if (Stream != null)
+                Stream.Close();
+            if (client != null)
+                client.Close();
+        }
+    }
+    public class ServerObject
+    {
+        static TcpListener tcpListener; // сервер для прослушивания
+        List<ClientObject> clients = new List<ClientObject>(); // все подключения
+
+        protected internal void AddConnection(ClientObject clientObject)
+        {
+            clients.Add(clientObject);
+        }
+        protected internal void RemoveConnection(string id)
+        {
+            // получаем по id закрытое подключение
+            ClientObject client = clients.FirstOrDefault(c => c.Id == id);
+            // и удаляем его из списка подключений
+            if (client != null)
+                clients.Remove(client);
+        }
+
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                    ClientObject clientObject = new ClientObject(tcpClient, this);
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+
+        }
+        protected internal void BroadcastMessage(string message, string id)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].Id != id) // если id клиента не равно id отправляющего
+                {
+                    clients[i].Stream.Write(data, 0, data.Length); //передача данных
+                }
+            }
+        }
+        // отключение всех клиентов
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop(); //остановка сервера
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close(); //отключение клиента
+            }
+            Environment.Exit(0); //завершение процесса
+        }
+    }
+}
